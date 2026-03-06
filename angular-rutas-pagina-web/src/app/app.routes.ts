399bdc2adb5619b7b461cd63ca2a456578093434
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-06 19:28:50 +0100

    Creamos el archivo de rutas en app.routes.ts

--- angular-rutas-pagina-web/src/app/app.routes.ts
+++ angular-rutas-pagina-web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { Error404Component } from '../pages/error404/error404.component';
+import { ContactoComponent } from '../pages/contacto/contacto.component';
+import { ProductosComponent } from '../pages/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', component: HomeComponent}, // para decir que es la ruta de entrada (el index.html)
+    {path: 'nuestros-servicios', component: ContactoComponent},
+    {path: 'productos', component: ProductosComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', component: Error404Component}
+
+];
---
+++ angular-rutas-pagina-web/src/pages/nuestros-servicios/nuestros-servicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuestros-servicios',
+  imports: [],
+  templateUrl: './nuestros-servicios.component.html',
+  styleUrl: './nuestros-servicios.component.css',
+})
+export class NuestrosServiciosComponent {
+
+}
