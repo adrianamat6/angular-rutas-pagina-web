743a4607705b35705b84cb77e9f9cacadaf986b2
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-07 11:51:22 +0100

    Creamos nuevo componente y le metemos ruta dinamica. Esta ruta dinamica no tiene la logica de si existe o no esa parte dinamica y siempre cargará el componente indicado

--- angular-rutas-pagina-web/src/app/app.routes.ts
+++ angular-rutas-pagina-web/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { Error404Component } from '../pages/error404/error404.component';
 import { ContactoComponent } from '../pages/contacto/contacto.component';
 import { ProductosComponent } from '../pages/productos/productos.component';
 import { NuestrosServiciosComponent } from '../pages/nuestros-servicios/nuestros-servicios.component';
+import { WorkViewComponent } from '../pages/work-view/work-view.component';
+
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', component: HomeComponent}, // para decir que es la ruta de entrada (el index.html)
     {path: 'home', component: HomeComponent},
     {path: 'nuestros-servicios', component: NuestrosServiciosComponent},
+    {path: 'nuestros-servicios/:seo', component: WorkViewComponent},
     {path: 'productos', component: ProductosComponent},
     {path: 'contacto', component: ContactoComponent},
     {path: '**', component: Error404Component}
---
+++ angular-rutas-pagina-web/src/app/db/nuestrosservicios.db.ts
@@ -0,0 +1,24 @@
+import { IWork } from "../interfaces/iwork";
+
+export const servicios: IWork[] = [
+    {
+        id: 1,
+        title: "Desarrollo Web",
+        description: "Creamos sitios web modernos y responsivos",
+        seo: "web"
+    },
+    {
+        id: 2,
+        title: "Consultoría",
+        description: "Asesoramiento profesional en tecnología",
+        seo: "consulting"
+    },
+    {
+        id: 3,
+        title: "Mantenimiento",
+        description: "Soporte y actualizaciones continuas",
+        seo: "maintenance"
+    }
+];
+
+
